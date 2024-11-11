ea8c4b00ea93ec08f2e3a88e1d608ab66baf53b5
Letícia Schiavon <leticiaschiavon20@gmail.com>
2024-11-11 11:53:41 -0400

    Adicionando Nova Tarefa na Lista

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ let tasks = [
     {id: 3, description: 'fazer o almoço', checked: false},
 ]
 
+const createTaskListItem = (task, checkbox) => {
+    const list = document.getElementById('todo-list');
+    const toDo = document.createElement('li');
+
+    toDo.id = task.id;
+    toDo.appendChild(checkbox);
+    list.appendChild(toDo);
+
+    return toDo;
+}
+
 const getCheckboxInput = ({id, description, checked}) => {
     const checkbox = document.createElement('input');
     const label = document.createElement('label');
@@ -12,7 +23,7 @@ const getCheckboxInput = ({id, description, checked}) => {
 
     checkbox.type = 'checkbox';
     checkbox.id = checkboxId;
-    checkbox.checked = checked;
+    checkbox.checked = checked || false;
 
     label.textContent = description;
     label.htmlFor = checkboxId;
@@ -25,17 +36,46 @@ const getCheckboxInput = ({id, description, checked}) => {
     return wrapper;
 }
 
+const getNewTaskId = () => {
+    const lastId = tasks[tasks.length -1]?.id;
+    return lastId ? lastId + 1 : 1;
+}
+
+const getNewTaskData = (event) => {
+    const description =  event.target.elements.description.value;
+    const id = getNewTaskId();
+
+    return {description, id}
+}
+
+const createTask = (event) => {
+    event.preventDefault();
+
+    const newTaskData = getNewTaskData(event);
+    //const {id, description} = newTaskData;
+
+    const checkbox = getCheckboxInput(newTaskData);
+    createTaskListItem(newTaskData, checkbox);
+
+    tasks = [
+        ...tasks, 
+        {id: newTaskData.id, description: newTaskData.description, checked: false }]; 
+}
+
 window.onload = function() {
+    const form = document.getElementById('create-todo-form');
+    form.addEventListener('submit', createTask);
+
     tasks.forEach((task) => {
         const checkbox = getCheckboxInput(task);
 
         const list = document.getElementById('todo-list');
         const toDo = document.createElement('li');
-        const button = document.createElement('button');
+        //const button = document.createElement('button');
 
         toDo.id = task.id;
         toDo.appendChild(checkbox);
-        toDo.appendChild(button);
+        //toDo.appendChild(button);
 
         list.appendChild(toDo);
     })
